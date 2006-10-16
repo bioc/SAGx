e182142c2864af0421a93bcd4ce1cfdd0b847c8d
unknown <pi.broberg@swipnet.se>
2006-10-16 20:26:44 +0000

    GSEA restandardisation
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SAGx@20605 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GSEA.mean.t.R
+++ R/GSEA.mean.t.R
@@ -1,5 +1,5 @@
-GSEA.mean.t <- function(data = X, samroc = samroc.res, probeset = probeset, pway = kegg, absolute = TRUE, two.side = FALSE, cutoff = c(5,Inf), B = 1000, smooth = FALSE){
-# Author : PBG 26OCT05, based on Tian et al. PNAS 2005.Modified 19MAY06
+GSEA.mean.t <- function(data = X, samroc = samroc.res, probeset = probeset, pway = kegg, type = c("original","absolute","maxmean"),  two.side = FALSE, cutoff = c(5,Inf), B = 1000, smooth = FALSE){
+# Author : PBG 26OCT05, based on Tian et al. PNAS 2005.Modified 19MAY06. Use of design slot added 16OCT06
 # data = X.Y; samroc = samroc.res;statistic = samroc.res$d;null.statistic = samroc.res$d0;pway = kegg;two.side = TRUE;probeset = rownames(X.Y);B <- 10000
 # data = matrix(rnorm(nrow(X.Y)*ncol(X.Y)), ncol = ncol(X.Y))
 # samroc.res <- samrocNboot(data = data, formula = ~as.factor(cl), B = 20)
@@ -8,10 +8,20 @@ GSEA.mean.t <- function(data = X, samroc = samroc.res, probeset = probeset, pway
 #formula <- samroc$formula 
 #contrast <- samroc$contrast
 if(smooth) stop("Smoothing not implemented yet.")
-if(!absolute) stop("Only absolute value of test statistic of test statistic currently used.")
-if(two.side) stop("Only one-sided test currently used base on absolute value of test statistic.")
-formula <- samroc@formula
+type <- match.arg(type) 
+if(type=="maxmean") stop("maxmean not implemented yet.")
+maxplus <- function(x) pmax(x, 0)
+maxminus <- function(x) -pmin(x, 0)
+maxmeanf <- function(x) { plusstat <- maxplus(x);minusstat <- maxminus(x);plusmean <- colMeans(plustat);minusmean <- colMeans(minusstat);
+                                        rowMax(cbind(plusmean, minusmean))}
+statistic <- switch(EXPR = type, absolute = abs(samroc@d), original = samroc@d, maxmean = maxmeanf(samroc@d))
+null.statistic <- switch(type, absolute = abs(samroc@d0), original = samroc@d0, maxmean = maxmeanf(samroc@d0))
+mean.statistic <- mean(statistic);sd.statistic <- sd(statistic) 
+mean.null.statistic <- mean(null.statistic);
+sd.null.statistic <- sd(null.statistic)
+
 contrast <- samroc@contrast
+if(is(data, "ExpressionSet")) data <- exprs(data)
 path.n = sapply(pway, length)
 pway = pway[(path.n > min(cutoff))&(path.n < max(cutoff))]
 used.probesets <- unique(unlist(pway));used.probesets <- used.probesets[used.probesets %in% probeset]
@@ -19,24 +29,20 @@ n.used <- sapply(pway, function(x) sum(used.probesets %in% x))
 pway <- pway[n.used > min(cutoff)]
 data <- as.matrix(data);rownames(data) <- probeset
 used.data <- data[probeset %in% used.probesets,]
-statistic = samroc@d[probeset %in% used.probesets]
-null.statistic = samroc@d0[probeset %in% used.probesets,]
-# probeset <- probeset[probeset %in% used.probesets]
+statistic = statistic[probeset %in% used.probesets]
+null.statistic = null.statistic[probeset %in% used.probesets,]
+probeset <- probeset[probeset %in% used.probesets]
 b <- B - ncol(null.statistic);
-sam <- model.matrix(formula)
-xmat <- model.matrix(formula)
-utest.design <- model.matrix(formula) 
+xmat <- samroc@design
 xhat <- solve(t(xmat)%*%xmat)%*% t(xmat)
-utest <- Xprep(indata = data, formula = formula, contrast = contrast)
-ss <- sqrt(utest$Vest/utest$k)
+ss <- samroc@se
 smoothinteger <- smooth+0
 npar <- qr(xmat)$rank      
 scalek <- (contrast%*%solve(t(xmat)%*%xmat)%*%contrast)^-1
 
-
 analysis <- function(x){
-   obs.stat <- mean(abs(statistic[used.probesets %in% x]));nulls <- null.statistic[used.probesets %in% x,]
-   x.data <- used.data[used.probesets %in% x,]
+   obs.stat <- mean(abs(statistic[probeset %in% x]));nulls <- null.statistic[probeset %in% x,]
+   x.data <- used.data[probeset %in% x,]
 #c-code  = bootstraploop
      result<-.C("newboot",
                 as.double(x.data),
@@ -59,9 +65,9 @@ analysis <- function(x){
                 
     diffs<-matrix(result$resultdiffs,ncol=b)
     sses<-matrix(result$resultsses,ncol=b)                
-    dstari <- diffs/(samroc@s0 + sses)
+    dstari <- diffs/(samroc@s0 + sses);dstari <- (dstari - mean.null.statistic)/ sd.null.statistic
     all.nulls <- cbind(dstari, nulls)
-    if(absolute) null.stats <- colMeans(abs(all.nulls)) else null.stats <- colMeans(all.nulls)
+    null.stats <- switch(EXPR = type, absolute = colMeans(abs(all.nulls)), original = colMeans(all.nulls), maxmean = maxmeanf(all.nulls))
     Fn <- ecdf(-abs(null.stats))
     pj <- (B*Fn(-abs(obs.stat))+1)/(B+1)
     pj
@@ -70,3 +76,4 @@ analysis <- function(x){
 out <- sapply(pway, analysis)
 out
 }
+
--- man/GSEA.mean.t.rd
+++ man/GSEA.mean.t.rd
@@ -7,15 +7,14 @@
 sets are ranked with respect to regulation as measured by an effect in a linear model using the SAM statistic.}
 
 \usage{GSEA.mean.t(data = X, samroc = samroc.res, probeset = probeset, 
-pway = kegg, absolute = TRUE, two.side = FALSE, cutoff = c(5,Inf), B = 1000, smooth = FALSE)}
+pway = kegg, type = c("original","absolute","maxmean"), two.side = FALSE, cutoff = c(5,Inf), B = 1000, smooth = FALSE)}
 
 \arguments{
-\item{data}{data matrix}
+\item{data}{data matrix or ExpressionSet}
 \item{samroc}{an object of class samroc.result}
-\item{probeset}{the Affymetrix identifiers}
+\item{probeset}{the Affymetrix identifiers or NULL, if data is ExpressionSet}
 \item{pway}{a list of pathways or gene sets}
-\item{absolute}{if TRUE the absolute value of the absolute value of the samroc test statistic is used. Currently this is
-the only option}
+\item{type}{if "absolute" value of the absolute value of the samroc test statistic is used. If "original" no transformation.}
 \item{two.side}{if TRUE a two-sided test is performed. Currently only one-sided test allowd}
 \item{cutoff}{Gene sets with the number of members not falling within the interval given by \emph{cutoff} are excluded}
 \item{B}{the number of permutations}
@@ -26,7 +25,7 @@ the only option}
 
 \value{A vector of p-values}
 
-\note{Experimental function}
+\note{Experimental function. Restandardization based on Efron and Tibshirani (2006) introduced.}
 
 \references{Tian, Lu and Greenberg, Steven A. and Kong, Sek Won and Altschuler, Josiah and Kohane, Isaac S. and Park, Peter J. 
 (2005) Discovering statistically significant pathways in expression profiling studies, \emph{PNAS} Vol. 102, nr. 38, pp. 13544-13549
@@ -35,3 +34,4 @@ the only option}
 
 
 \keyword{multivariate}
+
