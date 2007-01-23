5073afcb4db11fcec5e5a66b6cb747e5fd63c0e4
unknown <pi.broberg@swipnet.se>
2007-01-23 20:00:05 +0000

    update to JT.test
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SAGx@22169 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/JT.test.r
+++ R/JT.test.r
@@ -1,42 +1,85 @@
 # The Lehmann Nonparametrics: Statistical Methods Based on Ranks p. 233 #
 # Assumes that groups are coded in increasing numerical order #
+# The suggestions posted on R list by Christopher Andrews (SUNY Buffalo, Department of Biostatistics) are gratefully acknowledged
 
-JT.test <- function(data = utmat,class = g, labels = NULL){
+# new idea strsplit(as.character(tet(data = A)), split = "=")[[2]]
 
-n.obs <- ncol(data)
-
+JT.test <- function(data,class, labs = NULL, alternative = c("two-sided","decreasing","increasing"), ties = FALSE){
+# data <- Response;class <- Treatment;alternative = "two-sided"
+# decreasing means that the null hypothesis states that the trend is decreasing in higher class 
+alternative <- match.arg(alternative)
+calls <- strsplit(as.character(match.call()), split = "=")
+if(!is.ordered(class)) {
+     class <- as.ordered(class)
+     cat("class was not an ordered factor.  Redefined to be one.\n")
+   }
+if(is(data, "exprSet")) stop("Pls update data to ExpressionSet, see Help")
+if(is(data, "ExpressionSet")){ pDataX <- pData(data);class <- pDataX[,paste(class)];data <- exprs(data)}
+if(is.factor(data)) {data <- as.numeric(data);data <- as.matrix(data);factor.ind <- TRUE} else {factor.ind <- FALSE;data <- as.matrix(data)}
+if(!is.null(dim(data))) n.obs <- ncol(data) else n.obs <- length(data)
+if(!(n.obs == length(class))) {data <- t(data);n.obs <- ncol(data)}
 class.tab <- unique(class);class.tab <- class.tab[order(class.tab)]
-
-wilcox.stat <- function(x,y){
-        r <- rank(c(x, y))
-        n.x <- length(x)
-        n.y <- length(y)
-        STATISTIC <- sum(r[seq(along = x)]) - n.x * (n.x + 1) / 2  
-        return(STATISTIC)
-}
-
-
-sum.stat <- function(x,class){
-   sums <- 0
-   upper <- length(class.tab)-1
+if(min(dim(data))==1){
+sums <- 0
+  upper <- length(class.tab)-1
    for(i in 1:upper){
-      for(j in i+1:length(class.tab)){
-         sums <- sums+wilcox.stat(x[class==class.tab[i]],x[class==class.tab[j]])
+      for(j in seq(i+1,upper+1)){
+      x <- t(as.matrix(data[,class == class.tab[i]]));y <- t(as.matrix(data[,class == class.tab[j]]))
+      n.x <- ncol(x)
+      ranked.x <- rank(c(x,y))
+      r2= sum(ranked.x[1:n.x])
+      sums <- sums + r2 - n.x * (n.x + 1) / 2  
       }
    }
-sums                 
+} else {
+sums <- 0
+  upper <- length(class.tab)-1
+   for(i in 1:upper){
+      for(j in seq(i+1,upper+1)){
+      x <- as.matrix(data[,class == class.tab[i]]);y <- as.matrix(data[,class == class.tab[j]])
+      n.x <- ncol(x)
+      ranked.x <- t(as.matrix(apply(cbind(x,y),1,rank)))
+      r2= rowSums(as.matrix(ranked.x[,1:n.x]))
+      sums <- sums + r2 - n.x * (n.x + 1) / 2  
+      }
+   }
+}
+ni <- table(class)
+EH <- (n.obs^2 - sum(ni^2))/4
+if(ties == FALSE) STDH <-sqrt((n.obs^2*(2*n.obs + 3)-sum(ni^2*(2*ni + 3)))/72 ) else{
+                  dj <- list(apply(data, 1, table));term1 <- sapply(dj, function(x) sum(x*(x - 1)*(2*x + 5)));
+                  term2 <- sapply(dj, function(x) sum(x*(x - 1)*(x - 2)));term3 <- sapply(dj, function(x) sum(x*(x - 1))) 
+                  STDH <-sqrt(
+                  (n.obs*(n.obs - 1)*(2*n.obs + 5) - sum(ni*(ni - 1)*(2*ni + 5)) - term1)/72 +
+     sum(ni*(ni - 1)*(ni - 2)*term2)/(36*n.obs*(n.obs - 1)*(n.obs - 2)) +
+     sum(ni*(ni - 1))*term3/(8*n.obs*(n.obs - 1)))
 }
 
-EH <- (n.obs^2 - sum(table(class)^2))/4
+# continuity correction remains
 
-STDH <-sqrt((n.obs^2*(2*n.obs+3)-sum(table(class)^2*(2*table(class)+3)))/72 )
+ps <- pnorm((sums-EH)/STDH)
 
-utres <- t(apply(data, 1, function(x) c(2*min(pnorm((sum.stat(x,class)-EH)/STDH),1-pnorm((sum.stat(x,class)-EH)/STDH)),tapply(x,class,median),cor(rank(x),rank(class) ) )))
+pvalues <- switch(alternative,
+       "two-sided" = 2*pmin(ps,1-ps), "decreasing" = ps, "increasing" = 1-ps)
 
-colnames(utres)[c(1,ncol(utres))] <- c("trend p-value", "rank correlation");
+# pvalues <- 2*pmin(pnorm((sums-EH)/STDH),1-pnorm((sums-EH)/STDH))
 
-ifelse(is.null(labels),colnames(utres)[-c(1,ncol(utres))] <- class.tab, colnames(utres)[-c(1,ncol(utres))] <- labels)
-is.null(labels)
-#list(pvalues=utres[,1],medians=utres[,-c(1,ncol(utres))],trend = utres[,ncol(utres)])
+# utres <- t(apply(data, 1, function(x) c(2*min(pnorm((sum.stat(x,class)-EH)/STDH),1-pnorm((sum.stat(x,class)-EH)/STDH)),tapply(x,class,median),cor(rank(x),rank(class) ) )))
+
+medians <- t(apply(data, 1, function(x) c(tapply(x,class,median),cor(rank(x),rank(class) ) )))
+if(factor.ind) medians <- class.tab[medians,-ncol(medians)]
+# utres <- data.frame(pvalues, medians)
+if(is.null(labs)) level <- levels(class) else level <- labs
+alternative <- paste(alternative, paste(level,collapse=switch(alternative,
+                     two.sided = ", ", decreasing= " > ", increasing = " < ")), sep=": ")
+data.name <- paste(calls[[2]], "by", calls[[3]])
+ifelse(is.null(labs),colnames(medians)[-ncol(medians)] <- class.tab, colnames(medians)[-ncol(medians)] <- labs)
+colnames(medians)[ncol(medians)] <- "rank correlation"
+rownames(medians) <- rownames(data)
+utres <- list(statistic = sums, parameter = NULL, p.value = pvalues, method = "Jonckheere-Terpstra", 
+null.value = NULL, alternative = alternative, medians = medians, data.name = data.name)
+class(utres) <- c("JT-test","htest")
+names(utres$statistic) <- "J"
 return(utres)
 }
+
--- man/JT.test.rd
+++ man/JT.test.rd
@@ -1,4 +1,5 @@
 
+
 \name{JT.test}
 \alias{JT.test}
 \title{Jonckheere-Terpstra trend test}
@@ -6,21 +7,28 @@
 \description{The test is testing for a monotone trend in terms of the class parameter.
              The number of times that an individual of a higher class has a higher gene expression forms a basis for the inference.}
 
-\usage{trendA <- JT.test(data = datsA, class = g, labels = c("NS", "HS", "COPD0", "COPD1", "COPD2"))}
+\usage{trendA <- JT.test(data, class, labs = c("NS", "HS", "COPD0", "COPD1", "COPD2"), alternative = c("increasing", "decreasing", "two-sided"))}
 
 \arguments{
 \item{data}{A matrix with genes in rows and subjects in columns}
-\item{class}{the column labels}
+\item{class}{the column labels, if not an ordered fctor it will be redefined to be one.}
 \item{labels}{the labels of the categories coded by class}
 }
 
-\value{a matrix with the columns
-\item{pvalue}{the p-value for the two-sided test of no trend.}
+\value{an object of class JT-test, which extends the class htest, and includes the following slots
+\item{statistic}{the observed JT statistic}
+\item{parameter}{the null hypothesis parameter, if other value than 0.}
+\item{p.value}{the p-value for the two-sided test of no trend.}
+\item{method}{Jonckheere-Terpstra}
+\item{alternative}{The relations between the levels: decreasing, increasing or two-sided}
+\item{data.name}{the name of the input data}
 \item{median1 ... mediann}{the medians for the n groups}
 \item{trend}{the rank correlation with category}
 }
 
-\details{Assumes that groups are coded in incresing numerical order}
+\details{Assumes that groups are given in increasing order, if the class variable is not an ordered factor, it will be redefined to be one.
+
+The implementation owes to suggestions posted by to R list.}
 
 
 \examples{
@@ -31,12 +39,13 @@ g <- c(rep(1,5),rep(2,5),rep(3,5))
 # The groups have the medians
 tapply(A, g, median)
 # JT.test indicates that this trend is significant at the 5% level
-JT.test(data = t(A), class = g, labels = c("GRP 1", "GRP 2", "GRP 3"))
+JT.test(data = A, class = g, labels = c("GRP 1", "GRP 2", "GRP 3"), alternative = "two-sided")
 }
 
-\author{Per Broberg}
+\author{Per Broberg, acknowledging input from Christopher Andrews at SUNY Buffalo}
 
 \references{
 Lehmann, EH (1975) \emph{Nonparametrics: Statistical Methods Based on Ranks} p. 233. Holden Day}
 
 \keyword{nonparametric}
+
