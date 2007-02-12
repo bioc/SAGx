131638eeb2bc6c6bff6e4c89aab8de6d4118e4a4
unknown <pi.broberg@swipnet.se>
2007-02-12 18:27:51 +0000

    new version of GSEA.mean.t based on normal approximation with moments from permutation
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SAGx@22586 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GSEA.mean.t.R
+++ R/GSEA.mean.t.R
@@ -1,34 +1,39 @@
-GSEA.mean.t <- function(data = X, samroc = samroc.res, probeset = probeset, pway = kegg, type = c("original","absolute"),  two.side = FALSE, cutoff = c(5,Inf), B = 1000, smooth = FALSE){
-# Author : PBG 26OCT05, based on Tian et al. PNAS 2005.Modified 19MAY06. Use of design slot added 16OCT06, restandardisation 27OCT06
-# data = X; samroc = samroc.res; probeset = probeset; pway = kegg; type = "original"; two.side = FALSE; cutoff = c(5,Inf); B = 1000
-if(smooth) stop("Smoothing not implemented yet.")
+GSEA.mean.t <- function(samroc = samroc.res, probeset = probeset, pway = kegg, type = c("original","absolute", "maxmean"),  two.side = FALSE, cutoff = c(10,Inf), restand = TRUE){
+# Author : PBG 26OCT05, based on Tian et al. PNAS 2005.Modified 19MAY06. Use of design slot added 16OCT06, restandardisation 20OCT06
+# Version 07FEB07
+# data = golub; samroc = samroc.res; probeset = probeset; pway = kegg; type = "maxmean"; two.side = FALSE; cutoff = c(10,Inf); B = 1000;smooth = FALSE;restand = TRUE
+if(type == "maxmean") stop("maxmean not implemented yet.") 
 type <- match.arg(type) 
-if(type=="maxmean") stop("maxmean not implemented yet.") 
-
+if(is(data, "ExpressionSet")) data <- exprs(data)
+statistic <- samroc@d
+null.statistic <- samroc@d0
 # if maxmean restand should be done separately for s- and s+
-
-if(type=="maxmean") {
+if(type == "maxmean") {
    maxplus <- function(x) pmax(x, 0)
    maxminus <- function(x) -pmin(x, 0)
    s.plus <- maxplus(statistic)
-   s.minus <- max.minus(statistic)
+   s.minus <- maxminus(statistic)
    mean.plus.statistic <- mean(s.plus)
    mean.minus.statistic <- mean(s.minus)
    sd.plus.statistic <- sd(s.plus)
    sd.minus.statistic <- sd(s.minus) 
-   s.plus.null <- apply(samroc@d0, 2, maxplus)
-   s.minus <- max.minus(samroc@d)
+   s.plus.null <- maxplus(null.statistic)
+   s.minus.null <- maxminus(null.statistic)
    mean.plus.statistic <- mean(s.plus)
    mean.minus.statistic <- mean(s.minus)
    sd.plus.statistic <- sd(s.plus)
    sd.minus.statistic <- sd(s.minus) 
+   mean.plus.null.statistic <- mean(s.plus.null)
+   mean.minus.null.statistic <- mean(s.minus.null)
+   sd.plus.null.statistic <- sd(s.plus.null)
+   sd.minus.null.statistic <- sd(s.minus.null) 
    maxmeanf <- function(x) { plusstat <- maxplus(x);minusstat <- maxminus(x);
-                          plusmean <- colMeans(plustat);
-                          minusmean <- colMeans(minusstat);
+                          plusmean <- (colMeans(plustat)-mean.plus.statistic)/sd.plus.statistic;
+                          minusmean <- (colMeans(minusstat)-mean.minus.statistic)/sd.minus.statistic;
                           rowMax(cbind(plusmean, minusmean))}
 } else{
-   statistic <- switch(EXPR = type, absolute = abs(samroc@d), original = samroc@d)
-   null.statistic <- switch(EXPR = type, absolute = abs(samroc@d0), original = samroc@d0)
+   statistic <- switch(EXPR = type, absolute = abs(statistic), original = statistic)
+   null.statistic <- switch(EXPR = type, absolute = abs(null.statistic), original = null.statistic)
    mean.statistic <- mean(statistic);sd.statistic <- sd(statistic) 
    mean.null.statistic <- mean(null.statistic);
    sd.null.statistic <- mean(apply(null.statistic, 2, sd))
@@ -36,65 +41,52 @@ if(type=="maxmean") {
    restand.null.stat <- function(x) (x - mean.null.statistic)/sd.null.statistic   
 }
 
-contrast <- samroc@contrast
-if(is(data, "ExpressionSet")) data <- exprs(data)
 path.n = sapply(pway, length)
 pway = pway[(path.n > min(cutoff))&(path.n < max(cutoff))]
 used.probesets <- unique(unlist(pway))
 used.probesets <- used.probesets[used.probesets %in% probeset]
 n.used <- sapply(pway, function(x) sum(used.probesets %in% x))
 pway <- pway[n.used > min(cutoff)]
-data <- as.matrix(data);rownames(data) <- probeset
-used.data <- data[probeset %in% used.probesets,]
 statistic = statistic[probeset %in% used.probesets]
-statistic <- restand.stat(statistic)
+if(restand) statistic <- restand.stat(statistic)
 null.statistic = null.statistic[probeset %in% used.probesets,]
-null.statistic <- restand.null.stat(null.statistic)
+if(restand) null.statistic <- restand.null.stat(null.statistic)
 probeset <- probeset[probeset %in% used.probesets]
-b <- B - ncol(null.statistic);
-xmat <- samroc@design
-xhat <- solve(t(xmat)%*%xmat)%*% t(xmat)
-ss <- samroc@se
-smoothinteger <- smooth+0
-npar <- qr(xmat)$rank      
-scalek <- (contrast%*%solve(t(xmat)%*%xmat)%*%contrast)^-1
 
 analysis <- function(x){
-  obs.stat <- mean(statistic[probeset %in% x]);nulls <- null.statistic[probeset %in% x,]
-   x.data <- used.data[probeset %in% x,]
-#c-code  = bootstraploop
-     result<-.C("newboot",
-                as.double(x.data),
-                        as.integer(nrow(x.data)),
-                as.integer(ncol(x.data)),
-                        as.double(xhat),
-                        as.integer(nrow(xhat)),
-                as.integer(ncol(xhat)),
-                        as.double(xmat),
-                        as.double(contrast),
-                as.integer(smoothinteger),
-                  as.integer(b),
-                as.double(npar),
-                as.double(scalek),
-                as.double(ss),
-                resultdiffs=as.double(rep(0,(nrow(x.data))*b)),
-                        resultsses=as.double(rep(0,(nrow(x.data))*b)),
-                        length(contrast = 0),
-                PACKAGE = "SAGx" )                
-    diffs <- matrix(result$resultdiffs,ncol=b)
-    sses <- matrix(result$resultsses,ncol=b)                
-    dstari <- diffs/(samroc@s0 + sses);if(type=="absolute") dstari <- abs(dstari)
-    dstari <- restand.null.stat(dstari)
-    all.nulls <- cbind(dstari, nulls)
-    null.stats <- colMeans(all.nulls)
-    Fn <- ecdf(-abs(null.stats))
-    pj <- (B*Fn(-abs(obs.stat))+1)/(B+1)
-    pj
+# Note that statistic has been restandardised
+  gs.statistics <- statistic[probeset %in% x] 
+  N <- length(gs.statistics)
+  mean.stat <- mean(gs.statistics)
+  median.stat <- median(gs.statistics)
+  nulls <- null.statistic[probeset %in% x,]
+    null.stats <- colMeans(nulls);null.mean <- mean(null.stats);null.sd <- sd(null.stats)
+  if(type == "original"){ranked.nulls  <-  apply(nulls, 2, rank);
+  ranked.nulls <- ranked.nulls*(nulls > 0);rank.sums <- colSums(ranked.nulls)
+  rank.null.mean  <- mean(ranked.nulls)*N;rank.null.sd <- sd(rank.sums)
+  rank.stat <- sum(rank(gs.statistics)[gs.statistics > 0]);pw <- pnorm(rank.stat, mean = rank.null.mean, sd = rank.null.sd)
+  pw <- 2*pmin(pw, 1 - pw)}  
+#    pj <- (B*Fn(-abs(obs.stat))+1)/(B+1)
+    pj <- pnorm(mean.stat, mean = null.mean, sd = null.sd)
+    pj <- 2*pmin(pj, 1 - pj)
+    if(type == "original") list(c(pj, mean.stat, pw, median.stat))
+    else list(c(pj, mean.stat, median.stat))
+}
+out <- sapply(pway, analysis)
+names(out) <- names(pway)
+if(type == "original"){x1 <- sapply(out, function(x) x[1]);x2 <- sapply(out, function(x) x[2]);
+x3 <- sapply(out, function(x) x[3]);x4 <- sapply(out, function(x) x[4])
+out <- cbind(x1, x2, x3, x4);colnames(out) <- c("normal p-value", "mean statistic", "Wilcoxon p-value", "median statistic")}
+else {
+x1 <- sapply(out, function(x) x[1]);x2 <- sapply(out, function(x) x[2]);
+x3 <- sapply(out, function(x) x[3]);out <- cbind(x1, x2, x3)
+colnames(out) <- c("normal p-value", "mean statistic", "median statistic")
 }
 
-out <- sapply(pway, analysis)
-out
+return(out)
 }
+
+ 
 
  
  
--- man/GSEA.mean.t.rd
+++ man/GSEA.mean.t.rd
@@ -4,28 +4,30 @@
 \title{Gene Set Enrichment Analysis using output from samroc}
 
 \description{Based on a list of gene sets, e.g. pathways, in terms Affymtrix identifiers, these
-sets are ranked with respect to regulation as measured by an effect in a linear model using the SAM statistic.}
+sets are ranked with respect to regulation as measured by an effect in a linear model using the SAM statistic. Typical
+applications include two-group comparisons or simple linear regression to clinical variable or gene expression of a given gene.}
 
-\usage{GSEA.mean.t(data = X, samroc = samroc.res, probeset = probeset, 
-pway = kegg, type = c("original","absolute"), two.side = FALSE, cutoff = c(5,Inf), B = 1000, smooth = FALSE)}
+\usage{GSEA.mean.t(samroc = samroc.res, probeset = probeset, 
+pway = kegg, type = c("original","absolute"), two.side = FALSE, cutoff = c(5,Inf), restand = TRUE)}
 
 \arguments{
-\item{data}{data matrix or ExpressionSet}
 \item{samroc}{an object of class samroc.result}
 \item{probeset}{the Affymetrix identifiers or NULL, if data is ExpressionSet}
 \item{pway}{a list of pathways or gene sets}
 \item{type}{if "absolute" value of the absolute value of the samroc test statistic is used. If "original" no transformation.}
 \item{two.side}{if TRUE a two-sided test is performed. Currently only one-sided test allowd}
 \item{cutoff}{Gene sets with the number of members not falling within the interval given by \emph{cutoff} are excluded}
-\item{B}{the number of permutations}
-\item{smooth}{if TRUE smoothing of SEs is performed. Currently this is not permitted}
+item{restand}{if TRUE a 'restandardization' following Efron and Tibshirani (2006) is performed}
 }
 
 \author{Per Broberg}
 
-\value{A vector of p-values}
+\value{A matrix with columns normal approximation p-values, mean statistic, median statistic, and if type = "original", also 
+Wilcoxon signed ranks statistic based p-value.}
 
-\note{Experimental function. Restandardization based on Efron and Tibshirani (2006) introduced.}
+\details{Restandardization based on Efron and Tibshirani (2006) introduced. For normal approximation both the mean and the
+variance of the mean or Wilcoxon statistic is obtained from the permutation distribution included in the samroc.result object.
+Note that this will account for the dependency between genes.}
 
 \references{Tian, Lu and Greenberg, Steven A. and Kong, Sek Won and Altschuler, Josiah and Kohane, Isaac S. and Park, Peter J. 
 (2005) Discovering statistically significant pathways in expression profiling studies, \emph{PNAS} Vol. 102, nr. 38, pp. 13544-13549
